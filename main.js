8fa14b684b31308e2e45798ac538a898d1e8ccea
Mad1flin2 <75647159+Mad1flin2@users.noreply.github.com>
2021-09-01 12:36:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,27 @@ song="";
 function preload() {
 song=loadSound("music.mp3");
 }
+scoreRightWrist=0;
+scoreLeftWrist=0;
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
+function setup() {
+canvas=createCanvas(500,500);
+canvas.center();
+webcam=createCapture(VIDEO);
+webcam.hide();
+}
 function draw() { 
+webcam.center();
+image(webcam, 0, 0, width, width * webcam.height / webcam.width);
 }
 function play() {
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+song.play();
+song.setVolume(1);
+song.rate(1);
 }
 function stop() {
-    song.stop();
+song.stop();
 }
